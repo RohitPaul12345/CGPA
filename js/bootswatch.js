2436545877a079495fea2b0c8de7f631da02d8fe
Rohit Paul <113459757+RohitPaul12345@users.noreply.github.com>
2023-06-27 21:10:06 +0530

    Update bootswatch.js

--- js/bootswatch.js
+++ js/bootswatch.js
@@ -1,7 +1,7 @@
 (function(){
 
-  var $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
-    var html = $(this).parent().html();
+  let $button = $("<div id='source-button' class='btn btn-primary btn-xs'>&lt; &gt;</div>").click(function(){
+    let html = $(this).parent().html();
     html = cleanSource(html);
     $("#source-modal pre").text(html);
     $("#source-modal").modal();
@@ -18,12 +18,12 @@
   });
 
   function cleanSource(html) {
-    var lines = html.split(/\n/);
+    let lines = html.split(/\n/);
 
     lines.shift();
     lines.splice(-1, 1);
 
-    var indentSize = lines[0].length - lines[0].trim().length,
+    let indentSize = lines[0].length - lines[0].trim().length,
         re = new RegExp(" {" + indentSize + "}");
 
     lines = lines.map(function(line){
